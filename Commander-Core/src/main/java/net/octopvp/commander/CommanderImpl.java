936be905c3472d8a59e06d60d63ec19851058042
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-02-25 19:16:05 -0500

    Add CommandInfoProvider

--- Commander-Core/src/main/java/net/octopvp/commander/CommanderImpl.java
+++ Commander-Core/src/main/java/net/octopvp/commander/CommanderImpl.java
@@ -114,7 +114,9 @@ public class CommanderImpl implements Commander {
         registerProvider(CoreCommandSender.class, new SenderProvider());
         registerProvider(String.class, new StringProvider());
         registerProvider(String[].class, new StringArrayProvider());
+        registerProvider(CommandInfo.class, new CommandInfoProvider());
         registerCommandPreProcessor(context -> {
+            System.out.println("Preprocessor (perm)");
             CommandInfo commandInfo = context.getCommandInfo();
             CoreCommandSender sender = context.getCommandSender();
             if (commandInfo.isSubCommand()) {
@@ -128,6 +130,7 @@ public class CommanderImpl implements Commander {
             }
         });
         registerCommandPreProcessor(context -> { //Cooldown preprocessor
+            System.out.println("Preprocessor (cooldown)");
             if (context.getCommandInfo().cooldownEnabled() && context.getCommandInfo().isOnCooldown(context.getCommandSender().getIdentifier())) {
                 throw new CooldownException(context.getCommandInfo().getCooldownSeconds(context.getCommandSender().getIdentifier()));
             }
