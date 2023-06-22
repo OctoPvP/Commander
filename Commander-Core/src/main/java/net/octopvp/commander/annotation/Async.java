56315726ea2b948e502195412a410998fa441640
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-06-22 13:53:59 -0400

    make @Async distribute on methods

--- Commander-Core/src/main/java/net/octopvp/commander/annotation/Async.java
+++ Commander-Core/src/main/java/net/octopvp/commander/annotation/Async.java
@@ -31,6 +31,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
+@DistributeOnMethods
 public @interface Async {
     boolean value() default true;
 }
